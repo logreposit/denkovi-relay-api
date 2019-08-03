a1a9b76928e3992cabe0f7625750bff2670bb861
Dominic Miglar <dominic.miglar@netunix.at>
2019-08-03 17:21:38 +0200

    fixed request mapping

--- src/main/java/com/logreposit/denkovi/denkovirelayapi/rest/controllers/DenkoviRelayController.java
+++ src/main/java/com/logreposit/denkovi/denkovirelayapi/rest/controllers/DenkoviRelayController.java
@@ -6,16 +6,13 @@ import com.logreposit.denkovi.denkovirelayapi.services.DenkoviRelayService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@RestController("/api/v1/relays/")
+@RestController
+@RequestMapping("/api/v1/relays")
 @Validated
 public class DenkoviRelayController
 {
@@ -26,7 +23,7 @@ public class DenkoviRelayController
         this.denkoviRelayService = denkoviRelayService;
     }
 
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<List<Relay>> getAll()
     {
         List<Relay> relays = this.denkoviRelayService.get();
@@ -34,7 +31,7 @@ public class DenkoviRelayController
         return new ResponseEntity<>(relays, HttpStatus.OK);
     }
 
-    @PostMapping("/")
+    @PostMapping
     public ResponseEntity<List<Relay>> setAll(@RequestBody @Valid List<Relay> relays)
     {
         this.denkoviRelayService.set(relays);
