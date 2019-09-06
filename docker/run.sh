30410e4225ba9e47959c85996b0bfd40a699f484
Dominic Miglar <dominic.miglar@netunix.at>
2019-09-06 20:23:41 +0200

    changed project name

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Starting application ..."
-exec java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/ta-cmi-reader-service/app.jar
+exec java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/denkovi-relay-api/app.jar
 
