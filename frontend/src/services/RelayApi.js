4976a3d39a3748495a097c5749ecc28c8b8e507a
Dominic Miglar <dominic.miglar@netunix.at>
2020-02-03 20:21:35 +0100

    list / play procedures

--- frontend/src/services/RelayApi.js
+++ frontend/src/services/RelayApi.js
@@ -2,12 +2,18 @@ import axios from 'axios'
 
 export default {
     fetchRelayStates () {
-        return axios.get('')
+        return axios.get('/relays')
     },
     turnOnRelay (number) {
-        return axios.post('/' + number + '/on')
+        return axios.post('/relays/' + number + '/on')
     },
     turnOffRelay (number) {
-        return axios.post('/' + number + '/off')
+        return axios.post('/relays/' + number + '/off')
+    },
+    fetchProcedures () {
+        return axios.get('/procedures')
+    },
+    playProcedure (id) {
+        return axios.post('/procedures/' + id + '/actions/play')
     }
 }
