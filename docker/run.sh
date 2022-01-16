6206d33e9b982c7a2f05bb7743c5eb3407837e4a
Dominic Miglar <dominic.miglar@netunix.at>
2022-01-16 16:41:04 +0100

    run.sh: add JVM param --add-opens java.base/java.lang=ALL-UNNAMED

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting application ..."
-exec java -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/denkovi-relay-api/app.jar
+exec java --add-opens java.base/java.lang=ALL-UNNAMED -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/denkovi-relay-api/app.jar
